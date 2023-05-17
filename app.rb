da379fc7a3a3ada41ec21500cdd608a128352814
lyonbett <lyon.bett@student.moringaschool.com>
2023-05-17 12:05:32 +0300

    completed assignment

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
+puts "Hello World!"
 
+ pp [{ id: 1, hello: "World" }, 
+    { id: 2, hello: "Ruby" }, 
+    { id: 3, hello: "Moon" }, 
+    { id: 4, hello: "Learner" }]
 # p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
+ print "Pass this test, please."
+ p [1,2,3]
 
 # def add
     
